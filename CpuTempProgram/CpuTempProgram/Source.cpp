85cbe94688b4bb2a8512349f0aec96a878dba491
f18x <wombass@protonmail.com>
2018-07-02 01:09:57 +0200

    Testing Upload Script!

--- CpuTempProgram/CpuTempProgram/Source.cpp
+++ CpuTempProgram/CpuTempProgram/Source.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 int main() {
 	std::cout << "Hello World!" << std::endl; // Just a normal Hello World!!
-
+	std::cout << "Oh hi" << std::endl; // Hi!!
 	getchar();
 
 	return 0;
