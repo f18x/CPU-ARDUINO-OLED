46225f67db6c4678649f0cd13a1227aa4b761dff
Hackcraft <zakary@zakary.org>
2018-07-02 00:11:48 +0100

    Test

--- CpuTempProgram/CpuTempProgram/Source.cpp
+++ CpuTempProgram/CpuTempProgram/Source.cpp
@@ -2,6 +2,7 @@
 int main() {
 	std::cout << "Hello World!" << std::endl; // Just a normal Hello World!!
 	std::cout << "Oh hi" << std::endl; // Hi!!
+	std::cout << "Oh hello!" << std::endl;
 	getchar();
 
 	return 0;
