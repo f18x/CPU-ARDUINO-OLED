1c71829bb3e827a80fcde7062514d21705048e57
f18x <wombass@protonmail.com>
2018-07-02 11:16:22 +0200

    Adding comments

--- CpuTempProgram/CpuTempProgram/Source.cpp
+++ CpuTempProgram/CpuTempProgram/Source.cpp
@@ -1,15 +1,18 @@
-#include <iostream>
-#include <windows.h>
+// Todo 
+// Read CPU temp
+// Store temp in a string or anything else
+// Send CPU Temp trough serial to arduino
+
+#include <iostream> // input output
+#include <windows.h> // including windows.h
 
 void setup() {
-	SetConsoleTitle(TEXT ("CPU temperature")); 
+	SetConsoleTitle(TEXT ("CPU temperature")); // Can't go wrong with a good title
+
 }
 int main() {
-	setup();
-	// Todo 
-	// Read CPU temp
-	// Store temp in a string or anything else
-	// Send CPU Temp trough serial to arduino
+	setup(); // Running everything we put in setup 
+	
 				
 	getchar(); // Just a endline
 
